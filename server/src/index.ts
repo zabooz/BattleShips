ae21f6a6a598c487d8625626ddad3da75e52a1bc
Daniel Osojnik <zabooz1988@gmai.com>
2024-05-22 16:04:11 +0200

    renamed mermaid.md to clientServerInteraction.md

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+console.log("port", port);  
 app.get("/", (req, res) => {
   res.send("Hallo");
 });
@@ -10,3 +15,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
